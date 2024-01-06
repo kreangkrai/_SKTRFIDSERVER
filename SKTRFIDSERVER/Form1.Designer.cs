6548f3ce4ec2ae94fe3e46b75563b047159a4f96
Buddhism 1 <OpenglMan@hotmail.com>
2024-01-06 07:43:33 +0700

    cccc

--- SKTRFIDSERVER/Form1.Designer.cs
+++ SKTRFIDSERVER/Form1.Designer.cs
@@ -36,20 +36,22 @@ namespace SKTRFIDSERVER
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(58, 23);
+            this.label1.Location = new System.Drawing.Point(77, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 46);
+            this.label1.Size = new System.Drawing.Size(0, 58);
             this.label1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 91);
+            this.ClientSize = new System.Drawing.Size(383, 112);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "AUTO DUMP";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
